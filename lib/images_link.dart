96fb0f0191c06ae5075f08280679fd4c3072a9b1
Sahil-Tagline <sahilm.tagline@gmail.com>
2022-04-19 10:04:04 +0530

    Final Commit

--- lib/images_link.dart
+++ lib/images_link.dart
@@ -6,6 +6,4 @@ class images {
   static const String no_message = "assets/images/no_message.jpg";
   static const String no_order_image =
       "https://cdn.dribbble.com/users/1693462/screenshots/3504905/media/e76b879fc2bb9ec2a1f92da0732eb608.gif";
-  static const String no_order_image1 =
-      "https://cdn.dribbble.com/users/1693462/screenshots/3504905/media/e76b879fc2bb9ec2a1f92da0732eb608.gif";
 }
